eee7a3cdbe35ab37070a7c010068cd14285a74c5
prishajaiswal75 <164599457+prishajaiswal75@users.noreply.github.com>
2025-03-07 21:18:00 +0530

    Creating a file Shape.java
    The Shape class is an abstract base class that represents geometric shapes. It defines three protected attributes (dim_one, dim_two, dim_three) to store shape dimensions and num_sides for the number of sides. The constructor initializes these attributes, allowing subclasses to inherit and define specific shapes. The class also declares two abstract methods, calculateArea() and calculatePerimeter(), which must be implemented by any subclass to compute the respective values based on the shape's properties.

---
+++ Shape.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+abstract class Shape {
+    protected double dim_one;
+    protected double dim_two;
+    protected double dim_three;
+    protected int num_sides;
+
+    public Shape(double dim_one, double dim_two, double dim_three, int num_sides) {
+        this.dim_one = dim_one;
+        this.dim_two = dim_two;
+        this.dim_three = dim_three;
+        this.num_sides = num_sides;
+    }
+
+    abstract public double calculateArea();
+    abstract public double calculatePerimeter();
+}
+
